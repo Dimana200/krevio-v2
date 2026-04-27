4cbfe5f70f5e2f3b06d3519d1a908a9a740363e1
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-27 23:25:50 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function showPage() {}
+
+function toggleFullscreen() {}
+
+function handleOrientation() {}
+
+function bindAll() {}
+
+window.addEventListener('load', ...)
+
+window.addEventListener('popstate', ...)
