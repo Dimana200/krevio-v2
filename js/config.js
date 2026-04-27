bd16dc1c27484aa7d7f155e904862919c720056f
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-27 22:59:50 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,20 @@
+var STATE = {...}
+var CONFIG = {...}
+
+function fmt() {}
+function fmtBytes() {}
+function fmtTime() {}
+function fmtDur() {}
+
+function el() {}
+function mk() {}
+function ico() {}
+
+function showToast() {}
+
+function openModal() {}
+function closeModal() {}
+
+function bindSwipe() {}
+
+function parseTags() {}
