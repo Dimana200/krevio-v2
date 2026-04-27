30cc0c68a16b4eb5b8f7d79e33419334190a945e
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-28 00:06:42 +0200

    Update feed.js

--- js/feed.js
+++ js/feed.js
@@ -1,5 +1,5 @@
-function resetFeed() {}
-
-async function renderFeed() {}
-
-async function loadMoreVideos() {}
+async function loadFeed() {
+    const { data: videos } = await _supabase.from('videos').select('*, profiles(username, avatar_url)').order('created_at', { ascending: false });
+    const container = document.getElementById('video-feed');
+    container.innerHTML = videos.map(v => createVideoElement(v)).join('');
+}
