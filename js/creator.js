40520a3d98da697352a3d9d24b7fa8e87fbcd410
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-27 23:23:13 +0200

    Create creator.js

---
+++ js/creator.js
@@ -0,0 +1,9 @@
+async function toggleFollow() {}
+
+async function loadFollowing() {}
+
+async function getProfile() {}
+
+async function openCreatorProfile() {}
+
+async function loadCreatorPanel() {}
