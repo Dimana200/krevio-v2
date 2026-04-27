cd4e2cc3736947c0a5adc032bc9942ec83058c8d
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-27 23:14:02 +0200

    Create auth.js

---
+++ js/js/auth.js
@@ -0,0 +1,11 @@
+function switchAuth() {}
+
+async function doLogin() {}
+
+async function doRegister() {}
+
+async function doLogout() {}
+
+async function saveProfile() {}
+
+function toggleTheme() {}
