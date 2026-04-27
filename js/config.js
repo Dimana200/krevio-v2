343995fd59ea494e92c5fc787d4e018cc8a65354
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-28 00:45:44 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,6 @@
-const SUPABASE_URL = 'https://ofmbeukfctaxunfupmhi.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mbWJldWtmY3RheHVuZnVwbWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5MDYxNDAsImV4cCI6MjA1NTQ4MjE0MH0.9Mh-mO-R7L-yU-hN-n_y9G-P-r7-r-y_z_z_z_z_z'; // Пълният ключ от файла ти
+const CONFIG = {
+  URL: 'https://ofmbeukfctaxunfupmhi.supabase.co',
+  KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mbWJldWtmY3RheHVuZnVwbWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5MDYxNDAsImV4cCI6MjA1NTQ4MjE0MH0.9Mh-mO-R7L-yU-hN-n_y9G-P-r7-r-y_z_z_z_z_z',
+  MSG_COOLDOWN: 2000,
+  MSG_LIMIT: 50
+};
