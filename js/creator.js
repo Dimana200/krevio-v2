1bbd03ecbc51d6d12c57fee2aa8f23743b01c74a
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-28 00:50:40 +0200

    Update creator.js

--- js/creator.js
+++ js/creator.js
@@ -1,15 +1,9 @@
-function toggleCreatorMode() {
-    const isCreator = document.body.classList.toggle('creator-mode');
-    console.log("Creator mode:", isCreator);
-    // Допълнителни визуални промени за създатели
-}
-
-async function getAnalytics() {
-    if (!currentUser) return;
-    const { data, error } = await _supabase
-        .from('videos')
-        .select('likes, views')
-        .eq('user_id', currentUser.id);
-    
-    return data;
+async function loadCreatorDashboard() {
+  if (!STATE.user) return;
+  var { data: videos } = await _supabase.from('videos').select('*').eq('user_id', STATE.user.id);
+  if (videos) {
+    el('cp-val-videos').innerText = videos.length;
+    var totalLikes = videos.reduce((acc, v) => acc + (v.likes_count || 0), 0);
+    el('cp-val-likes').innerText = totalLikes;
+  }
 }
