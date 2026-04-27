ff5b2e757ef8335db5de74a6d7787a1af95834f4
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-28 00:05:38 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,28 @@
-function switchAuth() {}
+async function checkUser() {
+    const { data: { user } } = await _supabase.auth.getUser();
+    if (user) {
+        currentUser = user;
+        showMainApp();
+    } else {
+        showAuth();
+    }
+}
 
-async function doLogin() {}
+async function signUp(email, password, username) {
+    const { data, error } = await _supabase.auth.signUp({ email, password });
+    if (error) return alert(error.message);
+    const { error: profileError } = await _supabase.from('profiles').insert([{ id: data.user.id, username }]);
+    if (profileError) alert(profileError.message);
+    else alert('Регистрацията е успешна!');
+}
 
-async function doRegister() {}
+async function signIn(email, password) {
+    const { error } = await _supabase.auth.signInWithPassword({ email, password });
+    if (error) alert(error.message);
+    else checkUser();
+}
 
-async function doLogout() {}
-
-async function saveProfile() {}
-
-function toggleTheme() {}
+async function signOut() {
+    await _supabase.auth.signOut();
+    location.reload();
+}
