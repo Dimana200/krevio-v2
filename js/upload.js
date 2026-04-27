432f786cf33f04409acbdf55f343f684ef169e8f
Gcone <53370022+Dimana200@users.noreply.github.com>
2026-04-28 00:08:04 +0200

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -1,9 +1,9 @@
-function openUpload() {}
-
-function bindUpload() {}
-
-function setFile() {}
-
-function resetUpload() {}
-
-async function startUpload() {}
+async function uploadVideo(file) {
+    const fileName = `${Date.now()}_${file.name}`;
+    const { data, error } = await _supabase.storage.from('videos').upload(fileName, file);
+    if (error) return alert('Грешка при качване');
+    
+    const { data: { publicUrl } } = _supabase.storage.from('videos').getPublicUrl(fileName);
+    await _supabase.from('videos').insert([{ user_id: currentUser.id, url: publicUrl }]);
+    alert('Видеото е качено!');
+}
